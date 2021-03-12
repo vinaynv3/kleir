4355fda633d72c0fb2bc4a51b28672a71a975451
vinaynv3 <vinaynv987@gmail.com>
2021-03-11 23:32:00 -0500

    Report is completed

---
+++ Reports/templatetags/report_tags.py
@@ -0,0 +1,12 @@
+from django import template
+
+register = template.Library()
+
+@register.filter
+def formatSlug(slug,key):
+    slug = slug.split('_')
+    return slug[key].capitalize()
+
+@register.filter
+def getId(ids,key):
+    return ids[key]
--- Reports/views.py
+++ Reports/views.py
@@ -5,17 +5,40 @@ from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy, reverse
 from PropertyDocs.models import ClientInfo, BankRef
 
+
+"""
+Utility Class for BankRef & ClientInfo Model
+"""
+class DisplayDetails:
+
+    def __init__(self):
+        self.bank = None
+        self.date = None
+        self.ref = None
+        self.slug = None
+        self.ids = None
+
+    def create(self,Bankobject):
+        customer = ClientInfo.objects.get(pk=Bankobject.client_info_id)
+        self.bank,self.date,self.ref  = Bankobject.Bank_Type, Bankobject.Date, Bankobject.Reference_Number
+        self.ids = [customer.Client_ID,Bankobject.id,]
+        self.slug = customer.Slug
+        return self
+
+
+
 # Create your views here.
 def ReportsHomeView(request,*args,**kwargs):
     return render(request,'Reports/multi_filter_search.html')
 
 def getObjects(data):
 
-    if len(data) is 3:
-        bank_reports = BankRef.objects.filter(Date__gte =  data['fromDate']).filter( Date__lte= data['toDate']).filter(Bank_Type = Data['bank'])
+    if len(data) == 3:
+
+        bank_reports = BankRef.objects.filter(Date__gte =  data['fromDate']).filter( Date__lte= data['toDate']).filter(Bank_Type = data['bank'])
         return bank_reports
 
-    elif len(data) is 2:
+    elif len(data) == 2:
         date = list(data.keys())[1]
         bank_reports = BankRef.objects.filter(Date =  data[date]).filter( Bank_Type = data['bank'])
         return bank_reports
@@ -40,6 +63,8 @@ def ReportsDispalyView(request,*args,**kwargs):
     data = {k: v for k, v in data.items() if v != ''}
 
     if request.method == 'GET':
-
-        print(bank,fromDate,toDate)
-        return HttpResponse("Recieved")
+        Bankobjects = list(getObjects(data))
+        utility_class_list = [ DisplayDetails() for i in range(len(Bankobjects))]
+        report_display_list = [ utility_class_list[i].create(Bankobjects[i]) for i in range(len(Bankobjects))]
+        context = { 'objects':report_display_list, 'total_rows':len(report_display_list)}
+        return render(request,'Reports/multi_filter_search.html',context)
