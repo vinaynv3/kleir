11f31d1799d3a28e3ecd9e1198f38317f2012777
vinaynv3 <vinaynv987@gmail.com>
2021-03-28 19:58:04 -0400

    Templates config

--- kleir/settings.py
+++ kleir/settings.py
@@ -78,6 +78,11 @@ TEMPLATES = [
     },
 ]
 
+TEMPLATE_DIRS = (
+    os.path.join(PROJECT_DIR, "templates"),
+    # here you can add another templates directory if you wish.
+)
+
 WSGI_APPLICATION = 'kleir.wsgi.application'
 
 
