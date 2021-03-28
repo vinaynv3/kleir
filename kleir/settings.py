cf763957fd5a458295901462e246e84f78666719
vinaynv3 <vinaynv987@gmail.com>
2021-03-28 17:12:02 -0400

    Database config

--- kleir/settings.py
+++ kleir/settings.py
@@ -153,6 +153,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'ImageUpload/media')
 HEROKU: Postgres database URL
 """
 # Heroku: Update database configuration from $DATABASE_URL.
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+if os.getcwd() == '/app':
+    import dj_database_url
+    db_from_env = dj_database_url.config(conn_max_age=500)
+    DATABASES['default'].update(db_from_env)
