0fde64437fa1c425a34927c5da481c500890fca7
vinaynv3 <vinaynv987@gmail.com>
2021-03-28 18:00:30 -0400

    Postgre database config

--- kleir/settings.py
+++ kleir/settings.py
@@ -162,5 +162,14 @@ HEROKU: Postgres database URL
 # Heroku: Update database configuration from $DATABASE_URL.
 if os.getcwd() == '/app':
     import dj_database_url
+    DATABASES = {
+    'default': dj_database_url.config(default='postgres://localhost')
+    }
     db_from_env = dj_database_url.config(conn_max_age=500)
     DATABASES['default'].update(db_from_env)
+
+    # Honor the 'X-Forwarded-Proto' header for request.is_secure().
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+    # Allow all host headers
+    ALLOWED_HOSTS = ['*']
