0ded2a3cc52abacc6411cef1bd8740dbec832c8f
Vinay Venkatareddy <vinaynv987@gmail.com>
2021-03-28 17:24:15 -0400

    Update settings.py

--- kleir/settings.py
+++ kleir/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'kleir.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -95,7 +95,7 @@ DATABASES = {
                 'PORT': '3306',
     }
 }
-
+"""
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
